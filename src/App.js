bd64f249c90d2252a4e5a2187505a8429b27a967
Adrian <adriantutuianu01@gmail.com>
2022-02-10 22:09:41 +0000

    Created useState todos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 0, text: "buy milk", isCompleted: false },
+    { id: 1, text: "buy bread", isCompleted: true },
+  ]);
+
   return (
     <div className="App">
       <header className="App-header">
