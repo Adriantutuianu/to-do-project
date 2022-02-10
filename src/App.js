73d10ac2061b79e506e362cd937ac73ecc714f59
Adrian <adriantutuianu01@gmail.com>
2022-02-10 22:24:40 +0000

    Updated app.js and app.css ready to start

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+// import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 0, text: "buy milk", isCompleted: false },
-    { id: 1, text: "buy bread", isCompleted: true },
-  ]);
+  // const [todos, setTodos] = useState([
+  //   { id: 0, text: "buy milk", isCompleted: false },
+  //   { id: 1, text: "buy bread", isCompleted: true },
+  // ]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello react</h1>
     </div>
   );
 }
