27b575b1cb7a228a292c4ede9b5740f2dd962611
Adrian <adriantutuianu01@gmail.com>
2022-02-11 23:34:42 +0000

    Added styling and form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-// import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
+// import TodoList from "./components/TodoList";
 
 function App() {
-  // const [todos, setTodos] = useState([
-  //   { id: 0, text: "buy milk", isCompleted: false },
-  //   { id: 1, text: "buy bread", isCompleted: true },
-  // ]);
-
   return (
     <div className="App">
       <header>
         {" "}
         <h1>Adrian Todo List :</h1>
       </header>
+      <Form />
     </div>
   );
 }
