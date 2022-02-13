47011c542b2fb095fa7bd39ac810bd4a4c1576d0
Adrian <adriantutuianu01@gmail.com>
2022-02-13 17:28:01 +0000

    Fixed error key

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,12 @@ import React from "react";
 import Todo from "./Todo";
 
 const TodoList = ({ todos }) => {
-  console.log(todos);
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {/* For each object we can render out a Todo component :  */}
         {todos.map((todo) => (
-          <Todo text={todo.text} />
+          <Todo key={todo.id} text={todo.text} />
         ))}
       </ul>
     </div>
