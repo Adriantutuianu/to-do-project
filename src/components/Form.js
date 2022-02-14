ec0847136c79a15171a24e236e2099488c0377a7
Adrian <adriantutuianu01@gmail.com>
2022-02-14 22:38:58 +0000

    Added categories filter

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,6 +15,10 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
     setInputText("");
   };
 
+  const statusHandler = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <form>
       <input
@@ -27,7 +31,7 @@ const Form = ({ inputText, setInputText, todos, setTodos }) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
